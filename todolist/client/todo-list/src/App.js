95df7c72a06466728288231b5a54dd59ee7fbedd
khaledgaaieb <khaled.gaaieb@esprit.tn>
2023-02-13 23:39:07 +0100

    todo List template

--- todolist/client/todo-list/src/App.js
+++ todolist/client/todo-list/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form className="form">
+        <input type="text" placeholder="Add Todo Item" />
+        <button type="submit">Add</button>
+      </form>
+      <hr className="horLine" />
+      <div className="todo-listItems">
+        <div className="todo-item">
+          <li className="item-content">First to do ! </li>
+          <div className="todo-btns">
+            <button className="update-item">Update</button>
+            <button className="delete-item">Delete</button>
+          </div>
+        </div>
+
+        <div className="todo-item">
+          <li className="item-content">Second to do ! </li>
+          <div className="todo-btns">
+            <button className="update-item">Update</button>
+            <button className="delete-item">Delete</button>
+          </div>
+        </div>
+
+        <div className="todo-item">
+          <li className="item-content">Third to do ! </li>
+          <div className="todo-btns">
+            <button className="update-item">Update</button>
+            <button className="delete-item">Delete</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
